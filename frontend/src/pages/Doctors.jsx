3c1268a1530d6caa34dbd9ac7bf9d8cca209871a
Mohammed Musharraf <86351406+MohammedMusharraf11@users.noreply.github.com>
2024-09-28 16:30:34 +0530

    Added Backend dependecies and Doctors page

--- frontend/src/pages/Doctors.jsx
+++ frontend/src/pages/Doctors.jsx
@@ -1,26 +1,56 @@
-import React, { useContext } from 'react'
-import { useParams } from 'react-router-dom'
+import React, { useContext, useEffect } from 'react'
+import { useNavigate, useParams } from 'react-router-dom'
 
 const Doctors = () => {
     const { speciality } = useParams()
     const [filterDoc,setFilterDoc] = useState([])
+    const navigate = useNavigate();
+    
+    const applyFilter = ()=> {
+      if(speciality){
+        setFilterDoc(doctors.filterDoc(doc => doc.speciality==speciality))
+      }
+      else{
+        setFilterDoc(doctors)
+      }
 
+      useEffect(() =>{
+        applyFilter()
+      }, [doctors,speciality])
+    }
+
+    console.log(doctors);  // Check doctors data
+console.log(speciality);
     const {doctors}= useContext(AppContext)
   return (
     <div>
-      <p>Browse through the doctors specialist.</p>
-      <div>
-        <div>
-            <p>General physician</p>
-            <p>Gynecologist</p>
-            <p>Dermatologist</p>
-            <p>Pediatricians</p>
-            <p>Neurologist</p>
-            <p>Gastroenterologist</p>
+      <p className='text-gray-600'>Browse through the doctors specialist.</p>
+      <div className='flex flex-col sm:flex-row items-start gap-5 mt-5'>
+        <div className='flex flex-col gap-4 text-sm text-gray-600'>
+            <p onClick={() =>speciality=== 'General physician' ? navigate('/doctors') : navigate('/doctors/General physician')} className='{`w-[94vw] sm:w-auto pl-3 py-1.5 pr-16 border border-gray-300 rounded transition-all curson-pointer ${speciality === "General physician" ? "bg-indigo-100 text-black" :""}`}'>General physician</p>
+            <p onClick={() =>speciality=== 'Gynecologist' ? navigate('/doctors') : navigate('/doctors/Gynecologist')} className='{`w-[94vw] sm:w-auto pl-3 py-1.5 pr-16 border border-gray-300 rounded transition-all curson-pointer ${speciality === "Gynecologist" ? "bg-indigo-100 text-black" :""}`}'>Gynecologist</p>
+            <p onClick={() =>speciality=== 'Dermatologist' ? navigate('/doctors') : navigate('/doctors/Dermatologist')} className='{`w-[94vw] sm:w-auto pl-3 py-1.5 pr-16 border border-gray-300 rounded transition-all curson-pointer ${speciality === "Dermatologist" ? "bg-indigo-100 text-black" :""}`}'>Dermatologist</p>
+            <p onClick={() =>speciality=== 'Pediatricians' ? navigate('/doctors') : navigate('/doctors/Pediatricians')} className='{`w-[94vw] sm:w-auto pl-3 py-1.5 pr-16 border border-gray-300 rounded transition-all curson-pointer ${speciality === "Pediatricians" ? "bg-indigo-100 text-black" :""}`}'>Pediatricians</p>
+            <p onClick={() =>speciality=== 'Neurologist' ? navigate('/doctors') : navigate('/doctors/Neurologist')} className='{`w-[94vw] sm:w-auto pl-3 py-1.5 pr-16 border border-gray-300 rounded transition-all curson-pointer ${speciality === "Neurologist" ? "bg-indigo-100 text-black" :""}`}'>Neurologist</p>
+            <p onClick={() =>speciality=== 'Gastroenterologist' ? navigate('/doctors') : navigate('/doctors/Gastroenterologist')} className='{`w-[94vw] sm:w-auto pl-3 py-1.5 pr-16 border border-gray-300 rounded transition-all curson-pointer ${speciality === "Gastroenterologist" ? "bg-indigo-100 text-black" :""}`}'>Gastroenterologist</p>
         </div>
-        <div>
+        <div className='w-full grid gird-cols-auto gap-4 gap-y-6'>
             {
-                filterDoc.map()
+                filterDoc.map((item,index)=>(
+                  <div onClick={() => navigate(`/appointment/${item._id}`)} className='border border-gray-200 cursor-pointer'>
+                  <img className='bg-blue-50' src={item.image} alt="" />
+                  <div className='p-4'>
+                  <div className='flex items-center gap-2 text-sm text-center text-green-500'>
+                  <p className='w-2 h-2 bg-green-500 rounded-full'></p>
+                  <p>Available</p>
+                  </div>
+                  <p className='text-gray-900 text-lg font-medium'>{item.name}</p>
+                  <p className='text-gray-600 text-sm'>{item.speciality}</p>
+                </div>
+</div>
+
+                
+                ))
             }
 
         </div>
