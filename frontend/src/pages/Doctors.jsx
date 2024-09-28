0ece3730151c54dea9c453300b9e767725c475d1
Mohammed Musharraf <86351406+MohammedMusharraf11@users.noreply.github.com>
2024-09-28 15:10:07 +0530

    Update 3:10

---
+++ frontend/src/pages/Doctors.jsx
@@ -0,0 +1,32 @@
+import React, { useContext } from 'react'
+import { useParams } from 'react-router-dom'
+
+const Doctors = () => {
+    const { speciality } = useParams()
+    const [filterDoc,setFilterDoc] = useState([])
+
+    const {doctors}= useContext(AppContext)
+  return (
+    <div>
+      <p>Browse through the doctors specialist.</p>
+      <div>
+        <div>
+            <p>General physician</p>
+            <p>Gynecologist</p>
+            <p>Dermatologist</p>
+            <p>Pediatricians</p>
+            <p>Neurologist</p>
+            <p>Gastroenterologist</p>
+        </div>
+        <div>
+            {
+                filterDoc.map()
+            }
+
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Doctors
