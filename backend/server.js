15c1fe11133d875e26a7042c39bddbf1a135e835
Mohammed Musharraf <86351406+MohammedMusharraf11@users.noreply.github.com>
2024-09-29 00:05:00 +0530

    Added API for Admin Login and Adding Doctor

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import cors from 'cors'
 import 'dotenv/config'
 import connectDB from './config/mongodb.js'
 import connectCloudinary from './config/cloudinary.js'
+import adminRouter from './routes/doctorRoute.js'
 
 
 //  App config
@@ -17,6 +18,8 @@ app.use(express.json())
 app.use(cors())
 
 // API Endpoint
+app.use('/app/admin',adminRouter)
+// localhost: 4000/api/admin/add-doctor
 
 app.get('/',(req,res)=>{
     res.send('API Working YO')
