c616ec914a56083a7e0dd5abefa7c55f3ff0ec06
mdi-shifa <152677066+mdi-shifa@users.noreply.github.com>
2024-09-28 07:26:44 +0530

    commit

--- pages/MyProfile.jsx
+++ pages/MyProfile.jsx
@@ -79,19 +79,14 @@ return (
         </div>
 
         <div className='mt-10'>
-        <div>
             {
                 isEdit
                 ?<button className='border border-primary px-8 py-2 rounded-full hover:bg-primary hover:text-white transition-all' onClick={()=>setIsEdit(false)}>Save information</button>
-                :<button className='border border-primary px-8 py-2 rounded-full rounded-full hover:bg-primary hover:text-white transition-all' onClick={()=>setIsEdit(true)}>Edit</button>
+                :<button className='border border-primary px-8 py-2 rounded-full hover:bg-primary hover:text-white transition-all' onClick={()=>setIsEdit(true)}>Edit</button>
             }
         </div>
     </div>
 
-
-    
-    
-
 )
-
 }
+export default MyProfile
